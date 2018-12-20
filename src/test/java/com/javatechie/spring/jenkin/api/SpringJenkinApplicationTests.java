10c6d1a08ec41d78345219068d949bdd719d495b
basahota <techbasanta757@gmail.com>
2018-12-21 00:38:15 +0530

    first commit

---
+++ src/test/java/com/javatechie/spring/jenkin/api/SpringJenkinApplicationTests.java
@@ -0,0 +1,17 @@
+package com.javatechie.spring.jenkin.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJenkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
