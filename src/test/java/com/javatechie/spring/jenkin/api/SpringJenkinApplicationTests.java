8f087dc9d9e5810884edcc9330f97d8e16643652
basahota <techbasanta757@gmail.com>
2018-12-21 01:15:41 +0530

    added . for test

--- src/test/java/com/javatechie/spring/jenkin/api/SpringJenkinApplicationTests.java
+++ src/test/java/com/javatechie/spring/jenkin/api/SpringJenkinApplicationTests.java
@@ -17,7 +17,7 @@ public class SpringJenkinApplicationTests {
 	Logger logger=LoggerFactory.getLogger(SpringJenkinApplicationTests.class);
 	@Test
 	public void contextLoads() {
-		logger.info("Executing test package");
+		logger.info("Executing test package...");
 		assertEquals(true, true);
 	}
 
